6b224883b7d0f094e5ae7b65571911f612a18407
antony-palacio <84698466+antony-palacio@users.noreply.github.com>
2022-12-05 22:27:16 -0700

    Day 6 Start

---
+++ src/main/kotlin/AdventDay6.kt
@@ -0,0 +1,7 @@
+import java.io.File
+
+fun main() {
+    val fileName = "C:\\Users\\Tony\\IdeaProjects\\Hello_Kotlin\\AdventCode\\src\\main\\kotlin\\DataDay6"
+    val gameData = File(fileName).readLines()
+
+}
