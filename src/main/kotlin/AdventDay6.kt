ab232dd61356f62106570ce5e42ad6d71335b820
antony-palacio <84698466+antony-palacio@users.noreply.github.com>
2022-12-06 17:55:16 -0700

    Day 6 Solution

--- src/main/kotlin/AdventDay6.kt
+++ src/main/kotlin/AdventDay6.kt
@@ -1,7 +1,33 @@
 import java.io.File
+import java.util.stream.IntStream.range
 
 fun main() {
     val fileName = "C:\\Users\\Tony\\IdeaProjects\\Hello_Kotlin\\AdventCode\\src\\main\\kotlin\\DataDay6"
-    val gameData = File(fileName).readLines()
+    val gameData = File(fileName).readText()
+    val testCase = "mjqjpqmgbljsphdztnvjfqwrcgsmlb"
+    var subTestCase = listOf("")
+    fun partOneSolution() {
+        for (i in range(0, gameData.length - 4)) {
+            subTestCase = listOf(gameData.substring(i, i + 4)).map { it.toSet().toString() }
+            if (subTestCase[0].length == 12) {
+                print(i + 4) //Prints position of signal
+                break
+            }
+        }
+    }
+//    partOneSolution()
+
+    fun partTwoSolution() {
+        for (i in range(0, gameData.length - 14)) {
+            subTestCase = listOf(gameData.substring(i, i + 14)).map { it.toSet().toString() }
+            if (subTestCase[0].length == 42) {
+                println(i + 14) //Prints position of signal
+                break
+            }
+        }
+    }
+
+    partTwoSolution()
+//    println(subTestCase[0].length)
 
 }
